870023e1ccbf27492e45dd76b7dddeab7fffbdf6
MohammadJavad Rakeei <m.j.rakeei@gmail.com>
2026-01-29 05:23:57 +0000

    Fresh start: TechnikNet full installation script and project

---
+++ TechnikNet_system/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import set_language
+
+urlpatterns = [
+    path('i18n/setlang/', set_language, name='set_language'),
+]
+
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('properties.urls')),
+    prefix_default_language=False
+)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
